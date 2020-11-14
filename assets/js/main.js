4e3409d0b9a6ced58f7b8e15ef87ad533e14d3e3
Gerakas <andreasgerakianakis@gmail.com>
2020-11-14 07:08:34 +0000

    FINALISED THE LAYOUT AND MOST OF THE STYLING.
    The layout of the website has been completed. And the main features of the website have been added and can be seen.
    These include:
    
    - Search-bar to find lists by title.
    - Side-bar menu to take actions such as creating new lists and being able to view the "starred" lists and the lists the user has deleted.
    - Personalisation setting. So the user can customise the theme and change their name.
    
    These changes have been added to make it easier for the user to naviagate the site with more ease and to slightly customise it, which will add a more personal feel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     
     // Hambuger Icon toggle
-    $("#hamburger").click(function(){
+    $("#side-bar-toggle").click(function(){
         $("#side-bar").toggle();
         $("#main-viewport").toggleClass("col-xl-10 col-sm-9 col-xs-8");
     });
